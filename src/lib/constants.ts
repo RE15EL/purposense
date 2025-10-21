fd51a4c8109074d72e127664a8a3ad09deee3f4d
Reisel Valle <reiselvalle@gmail.com>
2025-10-20 21:02:52 -0400

    add certainty constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const CERTAINTY_COLORS = {
+  all: "bg-gray-100 text-gray-800 border-gray-300",
+  very_certain: "bg-green-100 text-green-800 border-green-300",
+  moderately_certain: "bg-yellow-100 text-yellow-800 border-yellow-300",
+  uncertain: "bg-red-100 text-red-800 border-red-300",
+};
+
+export const CERTAINTY_LABELS = {
+  all: "Select certainty",
+  very_certain: "Very certain",
+  moderately_certain: "Moderately certain",
+  uncertain: "Uncertain",
+};
